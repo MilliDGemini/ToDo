94da9799f990bef06c565625115e2e9824ca58ee
Milli D Gemini <86135909+MilliDGemini@users.noreply.github.com>
2021-08-30 11:38:04 -0500

    coding done & installing parcel

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqEBhw11FT9lfq6g9bPdpoLMubWouxFgg",
+  authDomain: "todo-812e2.firebaseapp.com",
+  projectId: "todo-812e2",
+  storageBucket: "todo-812e2.appspot.com",
+  messagingSenderId: "465941618736",
+  appId: "1:465941618736:web:ef4515c5248391bdd24df5",
+  measurementId: "G-EQQCT9PL5E",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
+var db = firebase.firestore();
